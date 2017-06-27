411b647f4d032e33deefdca290ebfb50b10d1498
Praijaval X. Gupta <Praijaval.Gupta@kp.org>
2017-06-27 16:50:07 -0700

    added new data

--- chef-repo/cookbooks/jenkins/recipes/default.rb
+++ chef-repo/cookbooks/jenkins/recipes/default.rb
@@ -6,3 +6,14 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+apt_update "update"
+
+apt_repository 'java' do
+  uri "ppa:webupd8team/java"
+  components ['main', 'stable']
+end
+
+package "oracle-java8-installer" do
+  action :install
+end
